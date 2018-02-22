dee087dc7f33416b51f92c9243783575464aa4dd
nick <lonelyrock21@gmail.com>
2018-02-22 21:28:43 +0200

    alagi stin ex8 n2

--- ex8.py
+++ ex8.py
@@ -4,11 +4,10 @@ am=0
 for i in range(30):
     nums.append(random.randint(-30,31))
 print nums
-j=2
-k=3
-for i in range(j):
-    for j in range(i+1,k):
-        for k in range(j+1,30):
+
+for i in range(28):
+    for j in range(i,29):
+        for k in range(j,30):
             if ((i!=j and  j!=k and k!=i)and(nums[i]+nums[j]+nums[k]==0)):
                 print nums[i],nums[j],nums[k]
                 am=am+1
